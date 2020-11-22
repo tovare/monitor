eccb3dc2da861464cee2e1622f748963176e5d25
Tov Are Jacobsen <mail@tovare.com>
2020-11-22 11:36:07 +0100

    Find last recoord and metadata

--- admin/tool.go
+++ admin/tool.go
@@ -4,10 +4,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
 	uptime "github.com/tovare/monitor"
+	"google.golang.org/api/iterator"
 
 	"cloud.google.com/go/bigquery"
 )
@@ -82,9 +84,81 @@ func createTestResultTable(projectID, datasetID, tableID string) error {
 	return nil
 }
 
+// printDatasetInfo demonstrates fetching dataset metadata and printing some of it to an io.Writer.
+func printDatasetInfo(w io.Writer, projectID, datasetID string) error {
+	// projectID := "my-project-id"
+	// datasetID := "mydataset"
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	meta, err := client.Dataset(datasetID).Metadata(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "Dataset ID: %s\n", datasetID)
+	fmt.Fprintf(w, "Description: %s\n", meta.Description)
+	fmt.Fprintln(w, "Labels:")
+	for k, v := range meta.Labels {
+		fmt.Fprintf(w, "\t%s: %s", k, v)
+	}
+	fmt.Fprintln(w, "Tables:")
+	it := client.Dataset(datasetID).Tables(ctx)
+
+	cnt := 0
+	for {
+		t, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		cnt++
+		fmt.Fprintf(w, "\t%s\n", t.TableID)
+	}
+	if cnt == 0 {
+		fmt.Fprintln(w, "\tThis dataset does not contain any tables.")
+	}
+	return nil
+}
+
+// findLastRecord finds the last record written to the bigquery table. This can be used
+// for incremental updates to the database.
+func findLastRecord(projectID, datasetID, tableID string) error {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	q := client.Query(`
+		SELECT max(testedtime) FROM homepage-961.monitor.uptime `)
+	it, err := q.Read(ctx)
+	if err != nil {
+		return err
+	}
+	for {
+		var values []bigquery.Value
+		err := it.Next(&values)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println(values[0])
+	}
+	return nil
+}
+
 func main() {
 	// createDataset("homepage-961", "monitor")
-	err := createTestResultTable("homepage-961", "monitor", "uptime")
+	// err := createTestResultTable("homepage-961", "monitor", "uptime")
+	// err := printDatasetInfo(os.Stdout, "homepage-961", "monitor")
+	err := findLastRecord("homepage-961", "monitor", "uptime")
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	cloud.google.com/go v0.72.0
 	cloud.google.com/go/bigquery v1.13.0
 	cloud.google.com/go/firestore v1.3.0
+	golang.org/x/tools v0.0.0-20201121010211-780cb80bd7fb // indirect
+	google.golang.org/api v0.35.0
 	google.golang.org/genproto v0.0.0-20201119123407-9b1e624d6bc4
 )
